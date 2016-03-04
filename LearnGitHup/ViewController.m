2142d200571f3f3a3169c874a4aa7d56aaf1cc00
scjy <383929022@qq.com>
2016-03-04 10:38:01 +0800

    在子分支修改

--- LearnGitHup/ViewController.m
+++ LearnGitHup/ViewController.m
@@ -5,7 +5,13 @@
 //  Created by 熊永静 on 16/3/4.
 //  Copyright © 2016年 静. All rights reserved.
 //
-
+ 
+ 项目上传
+ 1、新建一个工程
+ 2、点击+->add，让项目处于准备上传到本地状态。
+ 3、写好简介和详细描述，将项目提交到本地仓库
+ 4、点击publish
+ */
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 //  1.0版本已经完成
+//    项目在主分支修改
     
     
 }
